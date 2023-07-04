f83141213944e203dd9aa453c669c27b304b3dea
Jonatas <jonatasolmartins@outlook.com>
2023-07-04 01:53:59 -0300

    add some midleware.

--- go.mod
+++ go.mod
@@ -22,6 +22,7 @@ require (
 	github.com/gofiber/template v1.8.2 // indirect
 	github.com/gofiber/utils v1.1.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/joho/godotenv v1.5.1
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.16.5 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
@@ -33,6 +34,7 @@ require (
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
 	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
+	github.com/sujit-baniya/flash v0.1.8
 	github.com/tidwall/gjson v1.14.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
--- main.go
+++ main.go
@@ -1,29 +1,64 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"html/template"
+	"io/fs"
 	"log"
+	"os"
+	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/helmet"
+	"github.com/gofiber/fiber/v2/middleware/idempotency"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/template/django/v3"
+	"github.com/joho/godotenv"
 	"github.com/nbd-wtf/go-nostr"
 	"github.com/nbd-wtf/go-nostr/nip19"
+	"github.com/sujit-baniya/flash"
 )
 
 func main() {
+	app, err := initApp()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	app := initServer()
-
+	app.Use(helmet.New())
+	app.Use(idempotency.New())
 	app.Use(recover.New())
 
-	app.Get("/api/index", handleDefault)
+	app.Use(cors.New())
+	app.Use(logger.New(logger.Config{
+		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+	}))
 
-	app.Post("/api/create", handleCreate)
+	app.Static("/static", "./static")
+	// app.Use(favicon.New(favicon.Config{
+	// 	File: "./favicon.ico",
+	// 	URL:  "/favicon.ico",
+	// }))
 
-	log.Fatal(app.Listen(":3000"))
+	app.Use(WithFlash)
+
+	app.Get("/index", handleDefault)
+
+	account := app.Group("/account")
+	account.Post("/create", handleCreate)
+
+	feed := app.Group("/feed")
+	feed.Get("/", handleFeed)
+	feed.Get("won/:npub", handleFeed)
+	feed.Post("/post", func(c *fiber.Ctx) error { return c.Redirect("<h1>To be implemented</h1>") })
+
+	log.Fatal(app.Listen(os.Getenv("HTTP_LISTEN_ADDR")))
 }
 
 func GenerateKeys() (string, string) {
@@ -32,43 +67,63 @@ func GenerateKeys() (string, string) {
 	nsec, _ := nip19.EncodePrivateKey(sk)
 	npub, _ := nip19.EncodePublicKey(pk)
 
-	fmt.Println(nsec)
-	fmt.Println(npub)
-
 	return nsec, npub
 }
 
-func initServer() *fiber.App {
+func initApp() (*fiber.App, error) {
+	if err := godotenv.Load(); err != nil {
+		return nil, err
+	}
 
-	engine := initEngine()
+	engine := initEngineTemplate()
 
 	app := fiber.New(fiber.Config{
-		Views: engine,
-		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
-			code := fiber.StatusInternalServerError
-			//message := err.Error()
-
-			var e *fiber.Error
-
-			if errors.As(err, &e) {
-				code = e.Code
-			}
-
-			err = ctx.Status(code).SendFile(fmt.Sprintf("./views/errors/%d.html", code))
-
-			if err != nil {
-				return ctx.Status(fiber.StatusInternalServerError).SendString("Internal Server Error")
-			}
-
-			return nil
-		},
+		ErrorHandler:          ErrorHandler,
+		DisableStartupMessage: true,
+		PassLocalsToViews:     true,
+		Views:                 engine,
 	})
 
-	return app
+	return app, nil
 }
 
-func initEngine() *django.Engine {
+func ErrorHandler(ctx *fiber.Ctx, err error) error {
+	code := fiber.StatusInternalServerError
+
+	var e *fiber.Error
+	message := err.Error()
+	//TODO: remove this line
+	println(message)
+
+	if errors.As(err, &e) {
+		code = e.Code
+	}
+
+	err = ctx.Status(code).SendFile(fmt.Sprintf("./views/errors/%d.html", code))
+
+	if err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).SendString("Internal Server Error")
+	}
+
+	return nil
+}
+
+func initEngineTemplate() *django.Engine {
 	engine := django.New("./views", ".html")
+	engine.Reload(true)
+
+	engine.AddFunc("formatTime", func(t time.Time) string {
+		timeZero := time.Time{}
+		if t.Equal(timeZero) {
+			return "n/a"
+		}
+		return t.Format(time.DateTime)
+	})
+
+	fm := map[string]interface{}{
+		"lower": strings.ToLower,
+		"upper": strings.ToUpper,
+	}
 
 	engine.AddFunc("parseLineBreak", func(value interface{}) string {
 		if str, ok := value.(string); ok {
@@ -77,37 +132,85 @@ func initEngine() *django.Engine {
 		return ""
 	})
 
-	fm := map[string]interface{}{
-		"lower": strings.ToLower,
-		"upper": strings.ToUpper,
-	}
+	engine.AddFunc("css", func(name string) (res template.HTML) {
+		filepath.Walk("static/styles", func(path string, info fs.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.Name() == name {
+				res = template.HTML("<link rel=\"stylesheet\" href=\"/" + path + "\">")
+			}
+			return nil
+		})
+		return
+	})
 
 	engine.AddFuncMap(fm)
-
-	if err := engine.Load(); err != nil {
-		log.Fatal("load:", err)
-	}
 
 	return engine
 }
 
 func handleDefault(c *fiber.Ctx) error {
-	return c.Render("index", nil)
+	return c.Render("index", flash.Get(c))
 }
 
 func handleCreate(c *fiber.Ctx) error {
 	context := fiber.Map{
-		"greetings": "",
-		"npub":      "",
-		"nsec":      "",
+		"npub": "",
+		"nsec": "",
 	}
-
+	//TODO: remove this line
+	fmt.Println(c.FormValue("lkey"))
 	nsec, npub := GenerateKeys()
 
 	context["nsec"] = nsec
 	context["npub"] = npub
 
-	context["greetings"] = fmt.Sprintf("Hello %s", c.FormValue("fname"))
+	return flash.WithData(c, context).Redirect("/index")
+}
 
-	return c.Render("index", context)
+func handleFeed(c *fiber.Ctx) error {
+	ctx := context.Background()
+	relay, err := nostr.RelayConnect(ctx, "wss://relay.damus.io/")
+	if err != nil {
+		panic(err)
+	}
+
+	npub := "npub1ezyhugdszsnq4yjwxuy3ml8qnjmls3y3dz4p9h04tzhjyqmtv0xqxvmwhq"
+
+	var filters nostr.Filters
+	if _, v, err := nip19.Decode(npub); err == nil {
+		pub := v.(string)
+		filters = []nostr.Filter{{
+			Kinds:   []int{nostr.KindTextNote},
+			Authors: []string{pub},
+			Limit:   1,
+		}}
+	} else {
+		panic(err)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	sub, err := relay.Subscribe(ctx, filters)
+	if err != nil {
+		panic(err)
+	}
+
+	var content string
+	for ev := range sub.Events {
+		fmt.Println(ev.ID)
+		fmt.Println(ev.Content)
+		content = ev.Content
+		break
+	}
+
+	return c.Render("feed", fiber.Map{"content": content})
+}
+
+func WithFlash(c *fiber.Ctx) error {
+	values := flash.Get(c)
+	c.Locals("flash", values)
+	return c.Next()
 }
